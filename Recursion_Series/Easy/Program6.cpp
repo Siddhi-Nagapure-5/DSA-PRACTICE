ea64cbfa032e3329f6e5e7e8a9d839e1071bb628
Siddhi-Nagapure <siddhi.50it@gmail.com>
2024-12-07 15:34:56 +0530

    Problem 6

--- Recursion_Series/Easy/Program6.cpp
+++ Recursion_Series/Easy/Program6.cpp
@@ -1,16 +1,25 @@
-//Program 6
+//Program 6 Find Nth Fibonacci Number
 
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
+int printNthFibonacciNumber(int n)
+{
+    if(n<=1)
+    {
+     return n;
+    }
+    return printNthFibonacciNumber(n-1)+printNthFibonacciNumber(n-2);
+}
 int main()
 {
+   
+    int n;
+    cin >> n;
 
-
-
-
-
+    int result=printNthFibonacciNumber(n);
+    cout<<result;
 
     return 0;
 }
